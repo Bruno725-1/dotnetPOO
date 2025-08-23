3be4fe065d55e35e920d44b5e39afbedadd265f6
Bruno César Lima Araújo <bruuno.laraujo@gmail.com>
2025-08-22 21:19:21 -0300

    commit inicial, agora sim

--- projetoNivelamento/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-class Program
-{
-    static void Main(string[] args)
-    {
-        Ex4.Rodar();
-    }
-}
--- projetoNivelamento/nivelamentoHelper.cs
+++
@@ -1,8 +0,0 @@
-class NivelamentoHelper
-{
-    public static int Somar(int soma, int num) => soma + num;
-    public static bool Maior(int num, int maior) => num > maior;
-    public static bool Menor(int num, int menor) => num < menor && num >= 0;
-    public static int Contador(int num, int contador) => num >= 0 ? ++contador : contador;
-    public static double Media(int soma, int contador) => (double)soma / contador;
-}
