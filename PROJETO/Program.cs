6904a2363d22ed3f3055f6202821f2f7da865407
Bruno César Lima Araújo <bruuno.laraujo@gmail.com>
2025-08-22 22:09:28 -0300

    Primeiro commit, criação da classe ponto

---
+++ PROJETO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Ponto p1 = new Ponto();
+        Ponto p3 = new Ponto(25, 15, "Ponto p3");
+        p3.ImprimirCoordenadas();
+        double variavel = p3.GetX();
+        p3.SetX(45);
+        p3.SetY(55);
+        p3.SetDescricao("Esta é a nova descrição deste ponto");
+        p3.ImprimirCoordenadas();
+        Console.WriteLine(variavel);
+    }
+}
