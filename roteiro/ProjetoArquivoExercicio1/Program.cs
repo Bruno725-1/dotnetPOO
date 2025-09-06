5925d1e8079baba46dc26552a95ea27058725a77
Bruno César Lima Araújo <bruuno.laraujo@gmail.com>
2025-09-06 00:39:34 -0300

    Início do projeto

---
+++ roteiro/ProjetoArquivoExercicio1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+namespace ProjetoArquivoExercicio1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            StreamReader rd = new StreamReader(@"c:\Users\User\Documents\Faculdade\poo\dotnetPOO\roteiro\ProjetoArquivoExercicio1\customers-100.csv");
+
+            while (!rd.EndOfStream) { 
+                string linha = rd.ReadLine();
+                Console.WriteLine(linha); 
+                contador++;
+
+                if (contador%20==0)
+                {
+                    Console.WriteLine("\nDigite qualquer tecla para continuar...");
+                    Console.ReadKey();
+                }
+            }
+            rd.Close();
+
+            string text = "Um exemplo de string para tokenização.";
+            string[] tokens = text.Split(' ');
+            foreach (string token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+
+            Console.WriteLine("\nDigite qualquer tecla para continuar...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ roteiro/SysXYZ/lancamentoVo.cs
@@ -0,0 +1,15 @@
+/**
+* PUC Minas, Campus Barreiro.
+* POO Aula Prática 
+* Data: 05/09/2025 (Sexta´feira)
+* @author Bruno César Lima Araújo
+* Objetivo: Iniciar o RP4, o qual tem o objetivo de ver conceitos como manipulação de arquivos e persistência de objetos
+*/
+class LancamentoVo
+{
+    private int numero_conta;
+    private int codigo_agencia;
+    private double data_transacao;
+    private int codigo_transacao;
+    private double valor;
+}
