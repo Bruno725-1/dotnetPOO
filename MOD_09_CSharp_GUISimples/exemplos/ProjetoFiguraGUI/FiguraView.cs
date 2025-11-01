31ebb64978f5827093d375fd7b18ba2c7ae9b7c0
Bruno César Lima Araújo <bruuno.laraujo@gmail.com>
2025-11-01 01:07:03 -0300

    Execução do projeto que estava nos exemplos

---
+++ MOD_09_CSharp_GUISimples/exemplos/ProjetoFiguraGUI/FiguraView.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using controller;
+using model;
+
+namespace view
+{
+    public partial class FiguraView : Form
+    {
+        private ConjuntoHelper conjuntoHelper;
+
+        public FiguraView()
+        {
+            InitializeComponent();
+            conjuntoHelper = ConjuntoHelper.getInstance();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            FiguraVO figuraVO = null;
+            int codigo = 0;
+            string descricao;
+
+            try
+            {
+                codigo = int.Parse(txtCodigo.Text);
+
+            }
+            catch (Exception e2)
+            {
+                MessageBox.Show("Código inválido!");
+                return;
+            }
+            try
+            {
+                descricao = txtDescricao.Text;
+
+            }
+            catch (Exception e2)
+            {
+                MessageBox.Show("Descrição inválida!");
+                return;
+            }
+
+            figuraVO = new FiguraVO(codigo, descricao);
+
+            if (conjuntoHelper.adicionar(figuraVO) > 0)
+            {
+                MessageBox.Show("Objeto inserido com sucesso!");                
+            }
+            else
+            {
+                MessageBox.Show("Objeto não inserido!");
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            FiguraVO figuraVO = null;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ConjuntoHelper conjuntoHelper = ConjuntoHelper.getInstance();
+            FiguraVO figuraVO = null;
+
+            //retanguloVO = conjuntoHelper.pesquisar();
+
+        }
+    }
+}
---
+++ MOD_09_CSharp_GUISimples/exemplos/ProjetoFiguraGUI/ProjetoPrincipalMainView.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using view;
+
+namespace ProjetoFiguraGUI
+{
+    public partial class ProjetoPrincipalMainView : Form
+    {
+        private FiguraView retanguloView;
+        private RelatorioFiguraView figuraView;
+        
+
+        public ProjetoPrincipalMainView()
+        {
+            InitializeComponent();
+        }
+
+        private void manutençãoDeFigurasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (retanguloView == null)
+            {
+                retanguloView = new FiguraView();
+            }
+            retanguloView.ShowDialog();
+        }
+
+        private void todasAsFigurasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (figuraView == null)
+            {
+                figuraView = new RelatorioFiguraView();
+            }
+            figuraView.ShowDialog();
+        }
+    }
+}
